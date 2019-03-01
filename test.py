84f62b40e7919c55e7e86cad6166a4b673ae5dbe
liutie <34883317+zimugh@users.noreply.github.com>
2019-02-28 23:27:40 -0700

    Update test.py

--- test.py
+++ test.py
@@ -64,3 +64,7 @@ if __name__ == '__main__':
             print('processing (%04d)-th image... %s' % (i, img_path))
         save_images(webpage, visuals, img_path, aspect_ratio=opt.aspect_ratio, width=opt.display_winsize)
     webpage.save()  # save the HTML
+    
+    
+    
+haha
